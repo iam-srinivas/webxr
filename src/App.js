ddfd6c1a6046f96a65b5f25889aaeac145e4016d
unknown <srinivassrinivas478@gmail.com>
2021-05-27 13:00:51 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+import { ARCanvas, DefaultXRControllers } from '@react-three/xr'
+
+
+function App() {
+  return (
+    <div className="App">
+      <ARCanvas>
+        <DefaultXRControllers />
+      </ARCanvas>
+    </div>
+  );
+}
+
+export default App;
